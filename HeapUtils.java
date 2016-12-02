cc7ef178db6ff2af85cc55f723bec2427af6e386
Sahil Kapur <sahilkapur.a@gmail.com>
2016-12-02 14:46:01 -0800

    Fixed documentation

--- HeapUtils.java
+++ HeapUtils.java
@@ -4,12 +4,18 @@
 public class HeapUtils
 {
     /**
-     * Heapify is a recursive method that takes an array and brings the largest values to the top.
-     * To do this, it starts at the left-most non-leaf node. It then looks at the left and right child.
-     * If any of the children are bigger, it takes the larger value and exchanges places. This is done recursively,
-     * until the root node is the largest value. 
+     * Heapify gets the left child and the right child of a node and then compares them
+     * with the parent node to make sure that both of them have a smaller value than the
+     * parent. If at least one of the child has a larger value than the parent, the parent
+     * node will be swapped with the greater value of the two children. After the two
+     * values switch, heapify will then be used on the child node that was just modified to
+     * make sure that the tree is a heap. 
+
+     * @param   index   root of the tree that is being heapified
+     * @param   heap    array that contains the heap data
+     * @param   heapSize    Size of the heap
      * 
-     * Big-O:
+     * Big-O: n
      */
     public static void heapify(Comparable[] heap, int index, int heapSize)
     {
@@ -17,6 +23,10 @@ public class HeapUtils
 
     /**
      * Swaps two elements in an array
+     * 
+     * @param   heap        array that contains the heap data.
+     * @param   firstIndex  Index of the first node being swapped.
+     * @param   secondIndex Index of the second node being swapped.
      */
     private static void swap(Comparable[] heap, int firstIndex, int secondIndex)
     {
@@ -26,34 +36,43 @@ public class HeapUtils
     }
 
     /**
-     * Builds a heap from an array of randomly arranged values. The heap created will be in the order of a complete
-     * binary tree satisfying the rules of a heap.
+     * Build heap starts at the last non-leaf node then it applies heaapify to that node.
+     * Next, the method moves to the previous node to apply heapify. This process occurs
+     * recursively until the method reaches the root. The root must have the maximum value. 
      * 
-     * Big-O:
+     * @postcondition   Root will have the max value of the heap.
+     * 
+     * @param   heap    array that contains the heap data
+     * @param   heapSize    size of the heap
+     * 
+     * Big-O: n
      */
     public static void buildHeap(Comparable[] heap, int heapSize)
     {
     }
 
     /**
-     * Removes an item from the heap, specifically the root node of the heap. The new heap matches the heap rules
-     * and is a complete binary tree.
+     * Removes an item from the heap, specifically the root node of the heap. 
+     * The new heap matches the heap rules and is a complete binary tree.
+     * Swaps the root with the last leaf and reducces the heap size by one.
      * 
-     * Big-O:
+     * @param   heap        array that contains the heap data
+     * @param   heapSize    Size of the heap
+     * Big-O: n
      *
-    public static Comparable remove(Comparable[] heap, int heapSize)
-    {
-
-    }
      */
+    public static Comparable remove(Comparable[] heap, int heapSize)
+    {  
+        return true;
+    }
 
     /**
-     * Adds an item from the heap, matching the heap rules.
+     * Adds an item to the heap, creates a new array, copies all the values
      * 
      * Big-O:
-     * 
+     */
     public static Comparable[] insert(Comparable[] heap, Comparable item, int heapSize)
     {
+        return null;
     }
-     */
 }
